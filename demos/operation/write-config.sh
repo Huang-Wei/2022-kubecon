3692d4504f7fbeeb4367fbab5720a14398d42d70
Yuan Chen <yuan_chen3@apple.com>
2022-10-26 09:56:02 -0400

    Create write-config.sh

---
+++ demos/operation/write-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+KUBERNETES_DIR=/home/yuan/kubernetes
+cmd="$KUBERNETES_DIR/_output/bin/kube-scheduler --config=./scheduler-config.yaml --v=5 --write-config-to=./kube-scheduler-config.yaml"
+echo $cmd
+$cmd
